99743dda807f79d1b70853d59cdfea5505482273
qvbilam <qvbilam@163.com>
2024-08-05 14:03:01 +0800

    v1.0.0

--- initialize/router.go
+++ initialize/router.go
@@ -2,7 +2,6 @@ package initialize
 
 import (
 	"github.com/gin-gonic/gin"
-	"net/http"
 	"user/middleware"
 	userRouter "user/router"
 )
@@ -10,21 +9,9 @@ import (
 func InitRouters() *gin.Engine {
 	router := gin.Default()
 	router.Use(middleware.Cors()).Use(middleware.UserAgent())
-	router.GET("ping", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"msg": "pong",
-		})
-	})
-	router.GET("version", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"msg": "v1.0.0",
-		})
-	})
 	apiRouter := router.Group("")
 
 	// 初始化基础组建路由
 	userRouter.InitUserRouter(apiRouter)
-	userRouter.InitAccountRouter(apiRouter)
-
 	return router
 }
--- router/user.go
+++ router/user.go
@@ -2,6 +2,8 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
+	"user/api/account"
 	"user/api/user"
 	"user/middleware"
 )
@@ -9,12 +11,37 @@ import (
 func InitUserRouter(Router *gin.RouterGroup) {
 	ServerRouter := Router.Group("user")
 	{
+		// 直接响应
+		baseRouter := ServerRouter.Group("").Use(middleware.Cors())
+		{
+			baseRouter.GET("ping", func(ctx *gin.Context) {
+				ctx.JSON(http.StatusOK, gin.H{
+					"msg": "pong",
+				})
+			})
+			baseRouter.GET("version", func(ctx *gin.Context) {
+				ctx.JSON(http.StatusOK, gin.H{
+					"msg": "v1.0.0",
+				})
+			})
+		}
+
+		// 需要登陆
 		userAuthRouter := ServerRouter.Group("").Use(middleware.Cors()).Use(middleware.Auth()).Use(middleware.Trace())
 		{
-			userAuthRouter.GET("", user.Search) // todo
 			userAuthRouter.GET("/search", user.Search)
-			userAuthRouter.GET("/:id", user.Detail)
-			userAuthRouter.PUT("", user.Update)
+			userAuthRouter.GET("/homepage/:id", user.Detail)
+			userAuthRouter.PUT("/homepage", user.Update)
+			userAuthRouter.POST("/account/update", account.Update)
+		}
+
+		// 不需要登陆
+		accountRouter := ServerRouter.Group("").Use(middleware.Cors()).Use(middleware.Trace())
+		{
+			accountRouter.POST("/account/register", account.Register)
+			accountRouter.POST("/account/login", account.Login)
+			accountRouter.POST("/account/login/platform", account.LoginPlatform)
+			accountRouter.POST("/account/logout", account.Login).Use(middleware.Auth())
 		}
 	}
 }
