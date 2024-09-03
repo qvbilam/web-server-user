1e1ac699ca2a067746437db9709df832b580daf2
qvbilam <qvbilam@163.com>
2024-09-03 18:00:14 +0800

    jaeger

--- utils/jaeger.go
+++ utils/jaeger.go
@@ -7,7 +7,6 @@ import (
 	"github.com/opentracing/opentracing-go/ext"
 	"github.com/uber/jaeger-client-go"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/grpclog"
 	"google.golang.org/grpc/metadata"
 	"user/enum"
 )
@@ -92,35 +91,4 @@ func ClientInterceptor(tracer opentracing.Tracer) grpc.UnaryClientInterceptor {
 
 		return err
 	}
-}
-
-func ServerInterceptor(tracer opentracing.Tracer) grpc.UnaryServerInterceptor {
-	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
-		md, ok := metadata.FromIncomingContext(ctx)
-		if !ok {
-			md = metadata.New(nil)
-		}
-		spanContext, err := tracer.Extract(
-			opentracing.TextMap,
-			MDCarrier{md},
-		)
-
-		if err != nil && err != opentracing.ErrSpanContextNotFound {
-			grpclog.Errorf("extract from metadata err: %v", err)
-		} else {
-			span := tracer.StartSpan(
-				info.FullMethod,
-				ext.RPCServerOption(spanContext),
-				opentracing.Tag{Key: string(ext.Component), Value: "gRPC Server"},
-				ext.SpanKindRPCServer,
-			)
-			defer span.Finish()
-
-			ctx = opentracing.ContextWithSpan(ctx, span)
-		}
-
-		return handler(ctx, req)
-
-	}
-
 }
